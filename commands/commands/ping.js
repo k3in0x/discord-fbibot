9a38f69334b32b9b9e3aca4e5bde7e7a6896ac99
k3in0x <43535917+k3in0x@users.noreply.github.com>
2020-10-18 14:16:33 +0200

    Update ping.js

--- commands/commands/ping.js
+++ commands/commands/ping.js
@@ -1,18 +1,24 @@
 const { MessageEmbed } = require('discord.js');
+const Client = require('../structures/Client');
+const {Message} = require('discord.js');
 
 module.exports = {
     name: 'ping',
-    category: 'ping',
+    category: 'commands',
     description: 'Returns bot and API latency in milliseconds.',
     usage: `ping`,
+   /**
+    * @param {client} client
+    * @param {Message} message
+    * @param {String[]} args
+    */
     run: async (client, message, args) => {
-        const msg = await message.channel.send('🏓 Pinging...',{timeout: 5000});
-
-        const embed = new MessageEmbed()
-        .setColor(process.env.COLOR)
-        .setTitle('🏓 Pong!')
-        .setDescription(`Bot Latency is **${Math.floor(msg.createdTimestamp - message.createdTimestamp)} ms** \nAPI Latency is **${Math.round(client.ws.ping)} ms**`);
-        message.delete({timeout: 2000});
-        message.channel.send(embed,{timeout: 5000});
-    }
+      const msg = await message.channel.send(`Pinging...`)
+      await msg.edit(client.embed({
+        title: `🏓 Pong!`,
+        description: `Bot Latency is **${client.ws.ping}ms**\n Message edit latency is **${msg.createdAt - message.createdAt}ms**`
+      }, message));
+      await msg.edit("").then(msg => { msg.delete({timeout: 10000})});
+      message.delete({timeout: 2000});
+     }
 }
